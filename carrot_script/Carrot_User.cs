77d099e2052892e4ae4c85e2bb17504a4395b001
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-08-21 01:47:18 +0700

    Fix list avatar
    Fix list avatar

--- carrot_script/Carrot_User.cs
+++ carrot_script/Carrot_User.cs
@@ -3,7 +3,6 @@ using Firebase.Firestore;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
