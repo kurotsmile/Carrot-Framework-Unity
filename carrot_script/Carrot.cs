735ce4ee7bc002585c7a4d404567ac74fd983fdd
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-03-17 21:05:49 +0700

    Fix line in code
    Fix line in code

--- carrot_script/Carrot.cs
+++ carrot_script/Carrot.cs
@@ -1,10 +1,10 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
